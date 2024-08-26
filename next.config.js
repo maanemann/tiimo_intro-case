e3c9e8ced7fb8830951d2336b474c12ee927198c
maanemann <hans@niila.io>
2024-08-26 17:00:46 +0200

    Initiate next.js project, fetch data & render

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: [
+      'assets-global.website-files.com',
+      'tiimostrapistorage.blob.core.windows.net',
+      'uploads-ssl.webflow.com'
+    ]
+  },
+}
